09b2308fa9e0e010080cfc4e619261a5346e956c
YamaiKaguya <thfrewen@example.com>
2026-04-13 03:04:17 +0800

    test run

--- src/app/dashboard/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-// import Profile from "@/src/features/profile/profile"
-
-export default function CustomerPage() {
-   return (
-      <main>
-         {/* <Profile /> */}
-      </main>
-   )
-}
--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -2,10 +2,11 @@ import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
 export async function createClient() {
-   const cookieStore = await cookies(); // ← await here
+   const cookieStore = await cookies(); 
+
    return createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
       {
          cookies: {
             getAll() { return cookieStore.getAll(); },
